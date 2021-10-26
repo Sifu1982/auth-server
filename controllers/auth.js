31db9e65425c1a58a7460ee6b3fa1e00319b3e65
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-26 12:04:07 +0200

    feat: separate controllers from routes

---
+++ controllers/auth.js
@@ -0,0 +1,31 @@
+// Truco opcional para poder "tipar" res como response. Se importa response y se iguala res a response
+const { response } = require('express')
+
+
+const crearUsuario = (req, res = response) => {
+    return res.json({
+        ok: true,
+        msg: 'Crear usuario /new'
+    });
+}
+
+const loginUsuario = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: 'Login de usuario /'
+    });
+}
+
+const revalidarToken = (req, res) => {
+    return res.json({
+        ok: true,
+        msg: 'Renew'
+    });
+}
+
+
+module.exports = {
+    crearUsuario,
+    loginUsuario,
+    revalidarToken
+}
