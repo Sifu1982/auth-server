090dce9529f176c60d09e4db2ad75ac7a051d03a
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-28 16:04:05 +0200

    feat: create user in data base

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,51 @@
 // Truco opcional para poder "tipar" res como response. Se importa response y se iguala res a response
 const { response } = require('express');
+const Usuario = require('../models/Usuario')
 
 
-const crearUsuario = (req, res = response) => {
+const crearUsuario = async (req, res = response) => {
     // console.log(req.body);
 
 
     const { email, name, password } = req.body;
     // console.log(email, name, password);
 
-    return res.json({
-        ok: true,
-        msg: 'Crear usuario /new'
-    });
+    try {
+
+        // Verificar el email
+        const usuario = await Usuario.findOne({ email });
+
+        if (usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: 'El usuario ya existe con ese email'
+            });
+        }
+
+        // Crear usuario con el modelo
+        const dbUser = new Usuario(req.body);
+
+        // Hashear o encriptar la contraseña
+
+        // Generar el JWT (Jason Web Token)
+
+        // Crear usuario de DB
+        await dbUser.save();
+
+        // Generar respuesta exitosa
+        return res.status(201).json({
+            ok: true,
+            uid: dbUser.id,
+            name,
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            msg: 'Por favor hable con el administrador'
+        });
+    }
+
 }
 
 const loginUsuario = (req, res = response) => {
