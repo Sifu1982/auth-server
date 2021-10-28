1cfc4ee622d2267a3d6a25d90908dd0f359d12ee
David Martínez Trigo <davidmtrigo@hotmail.es>
2021-10-28 09:48:37 +0200

    feat: create validators and response with errors message

--- routes/auth.js
+++ routes/auth.js
@@ -5,11 +5,22 @@ const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/a
 
 const router = Router();
 
-// Crear un nuevo usuario. NOTA: el callback se llama "controlador"
-router.post('/new', crearUsuario);
+// Crear un nuevo usuario. NOTA: el callback crearUsuario se llama "controlador"
+router.post('/new',
+    // Array de middlewares -> Validadores que tienen que cumplir las rutas para ejecutarse
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email')
+            .notEmpty().withMessage('El email es obligatorio')
+            .isEmail().withMessage('El email no tiene un formato correcto'),
+        check('password')
+            .notEmpty().withMessage('La contraseña no puede venir vacía')
+            .isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres')
+    ]
+    , crearUsuario);
 
 // Login de usuario
-router.post('/', [  // Array de middlewares -> Validadores que tienen que cumplir las rutas para ejecutarse
+router.post('/', [
     check('email', 'El email es obligatorio').isEmail(),
     check('password', 'La contraseña es obligatoria').isLength({ min: 6 })
 ], loginUsuario);
